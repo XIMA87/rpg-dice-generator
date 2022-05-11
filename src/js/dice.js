cc5434b6f01bdba417f97d600c58baeddf73f9bf
Pedro Luiz Braga <91227083+XIMA87@users.noreply.github.com>
2022-05-11 12:52:21 -0300

    Add files via upload

---
+++ src/js/dice.js
@@ -0,0 +1,59 @@
+let resultado = document.getElementById('resultado')
+let aleatorios = document.getElementById('aleatorios')
+
+
+function d4() {
+    
+    let d4 = Math.ceil(Math.random() * 4);
+
+    let aleatorios = document.getElementById('aleatorios').innerHTML = 'RESULTADO';
+    resultado.innerHTML = d4;
+}
+
+function d6() {
+    
+    let d6 = Math.ceil(Math.random() * 6);
+
+    let aleatorios = document.getElementById('aleatorios').innerHTML = 'RESULTADO';
+    resultado.innerHTML = d6;
+}
+
+function d8() {
+    
+    let d8 = Math.ceil(Math.random() * 8);
+
+    let aleatorios = document.getElementById('aleatorios').innerHTML = 'RESULTADO';
+    resultado.innerHTML = d8;
+}
+
+function d10() {
+    
+    let d10 = Math.ceil(Math.random() * 10);
+
+    let aleatorios = document.getElementById('aleatorios').innerHTML = 'RESULTADO';
+    resultado.innerHTML = d10;
+}
+
+function d12() {
+    
+    let d12 = Math.ceil(Math.random() * 12);
+
+    let aleatorios = document.getElementById('aleatorios').innerHTML = 'RESULTADO';
+    resultado.innerHTML = d12;
+}
+
+function d20() {
+    
+    let d20 = Math.ceil(Math.random() * 20);
+
+    let aleatorios = document.getElementById('aleatorios').innerHTML = 'RESULTADO';
+    resultado.innerHTML = d20;
+}
+
+function d100() {
+    
+    let d100 = Math.ceil(Math.random() * 100);
+
+    let aleatorios = document.getElementById('aleatorios').innerHTML = 'RESULTADO';
+    resultado.innerHTML = d100;
+}
